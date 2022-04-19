f9401e1964bb526a975fbbdd1063f044818236bb
RonaldoNeves <ronaldojuniorpa@gmail.com>
2022-04-18 22:25:35 -0300

    teste 01 pycharm 18-04

---
+++ ex004.py
@@ -0,0 +1,8 @@
+n= input('dgte algo')
+print('o tpo prmtvo desse valor é', type(n))
+print('ele é alfanumerico? ', n.isalnum())
+print('é um espaço? ', n.isspace())
+print('está atribuído ao teclado? ', n.isascii())
+print('', n.isprintable())
+
+
---
+++ ex005.py
@@ -0,0 +1,2 @@
+n1 = int(input('um numero'))
+print('o numero anterior é {}, e posterior {}'.format(n1-1, n1+1))
